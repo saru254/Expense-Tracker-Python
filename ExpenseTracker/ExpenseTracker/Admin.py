c772f22c1cead1df0e2ae976f8369a24a8e2ec4b
Karen Meni <107751782+saru254@users.noreply.github.com>
2024-09-24 19:35:26 +0300

    Update Admin.py

---
+++ ExpenseTracker/ExpenseTracker/Admin.py
@@ -0,0 +1,14 @@
+#register your models here
+
+from .models import Addmoney_info
+From django.contrib import admin
+
+class Addmoney_infoAdmin(admin.ModelAdmin):
+    list_display = ("user", "quantity", "Date", "Category", "add_money")
+
+admin.site.register(Addmoney_info,Addmoneu_infoAdmin)
+
+from django.contrib.sessions.models import Session
+admin.site.register(Session)
+from .model import UserProfile
+admin.site.register(UserProfile)
