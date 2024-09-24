83445871bada635b7ba82f9f101e034a19cf2c8b
Karen Meni <107751782+saru254@users.noreply.github.com>
2024-09-24 18:41:01 +0300

    Update models.py

--- ExpenseTracker/ExpenseTracker/models.py
+++ ExpenseTracker/ExpenseTracker/models.py
@@ -1,5 +1,8 @@
 from django.db import models # type: ignore
 from django.utils.timezone import now # type: ignore
-from django.contrib.auth.models import User
-from django.conf import settings
-from django.db.models.signals import post_save
+from django.contrib.auth.models import User # type: ignore
+from django.conf import settings # type: ignore
+from django.db.models.signals import post_save # type: ignore
+from django.dispatch import receiver # type: ignore
+from django.db.models import Sum # type: ignore
+
