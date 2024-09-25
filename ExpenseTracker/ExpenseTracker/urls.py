af2b861113d14adba719ed9af98328b0a1458dd9
Karen Meni <107751782+saru254@users.noreply.github.com>
2024-09-25 13:51:11 +0300

    Update urls.py

--- ExpenseTracker/ExpenseTracker/urls.py
+++ ExpenseTracker/ExpenseTracker/urls.py
@@ -15,7 +15,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path ('', views.home, name='home'),
+    path('index/', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('handleSignup/', views.handleSignup, name = 'handleSignup'),
+    path('handlelogin/', views.handlelogin, name = 'handlelogin'),
+    path('handleLogout/', views.handleLogout, name = 'handleLogout'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name = "home/reset_password.html"), name = 'reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name = "home/reset_password_sent.html"), name='password_reset_done'),
+    path('reset/<uidb64>/token>/', auth_views.PasswordResetConfirmView.as_view(template_name= "hoem/password_reset_form.html"), name = 'password_reset_done'),
+    path('reset_password_complete/', auth_views.PasswordResetView.as_view(template_name= "home/password_reset_done.html"), name = 'password_reset_complete'),
+
+    path('addmoney/', views.addmoney, name='addmoney'),
+
+    path('addmoney_submission/', views.addmoney_submission, name = 'addmoney_submission'),
+
+    path('charts/', views.charts,name = 'charts'),
+    path('tables/', views.tables, name = 'tables'),
+    path('expense_edit/<int:id>', views.expense_edit, name = 'expense_edit'),
+    path('<int:id>/addmoney_update/', views.addmoney_update, name = "addmoney_update"),
+    
 ]
