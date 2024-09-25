cc7a56f453fe489ffcf05411b844fc4784bad053
Karen Meni <107751782+saru254@users.noreply.github.com>
2024-09-25 14:37:37 +0300

    Update views.py

--- ExpenseTracker/ExpenseTracker/views.py
+++ ExpenseTracker/ExpenseTracker/views.py
@@ -12,3 +12,25 @@ from django.db.models import Sum
 from django.http import JsonResponse
 import datetime
 from django.utils import timezone
+
+def home(request):
+    if request.session.has_key('is_logged'):
+        return redirect('/index')
+    return render (request, 'home/login.html')
+
+def index(request):
+    if request.session.has_key('is_logged'):
+        user_id = request.session["user_id"]
+        user = User.objects.get(id=user_id)
+
+        addmoney_info = Addmoney_info.objects.filter(user=user).order_by('-Date')
+        paginator = Paginator(addmoney_info, 4)
+        page_number = request.GET.get('page')
+
+        page_obj = Paginator.get_page(paginator, page_number)
+        context = {
+            'page_obj' : page_obj
+        }
+
+        return render(request, 'home/index.html', context)
+    return redirect('home')
